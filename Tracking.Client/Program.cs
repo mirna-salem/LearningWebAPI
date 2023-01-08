91361c132a67a39ddda7c1e1029749f993580d64
mirna-salem <ms.mirnasalem@gmail.com>
2023-01-08 17:09:09 -0600

    Call Web API from .NET Client.

---
+++ Tracking.Client/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using Tracking.Client;
+
+HttpClient client = new();
+client.BaseAddress = new Uri("https://localhost:44375");
+client.DefaultRequestHeaders.Accept.Clear();
+client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+HttpResponseMessage reponse = await client.GetAsync("api/issue");
+reponse.EnsureSuccessStatusCode();
+
+if (reponse.IsSuccessStatusCode)
+{
+    var issues = await reponse.Content.ReadFromJsonAsync<IEnumerable<IssueDto>> ();
+    foreach (var issue in issues)
+    {
+        Console.WriteLine(issue.Title);
+        Console.WriteLine(issue.Description);
+    }
+}
+else
+{
+    Console.WriteLine("No results");
+}
+
+Console.ReadLine();
